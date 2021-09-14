3f6d055946e6bf4453532b87a3911a647522ed20
Emilly Wasson <emillywasson@knights.ucf.edu>
2021-09-13 21:23:47 -0400

    Thank you kindly for reviewing my code :)

---
+++ prog2.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Emilly Wasson
+ */
+
+import java.util.Scanner;
+
+public class prog2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the input string? ");
+        String s = input.next();
+        System.out.println(s.length());
+    }
+}
